3c3f66101aad2f7c7bef568c035fcf53259625d0
greniu <realgreniu@proton.me>
2025-05-16 14:05:36 +0200

    podłączenie styli css

--- uzytkownicy.php
+++ uzytkownicy.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal Społecznościowy</title>
+    <link rel="stylesheet" href="styl5.css">
 </head>
 <body>
     
